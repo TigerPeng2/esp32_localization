5713bb8c82c2d261eccffb3e6f29c94ab222cd6f
Tiger Peng <nytigeryxp@gmail.com>
2024-11-30 21:57:34 -0500

    Actual initial commit

---
+++ process_log.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    file_name = input("Enter the file name: ")
+    with open(file_name, "r") as file:
+        with open(f'{file_name.split(".")[0]}_list.csv', 'w') as output_file:
+            lines = file.readlines()
+            for line in lines:
+                if "ftm_station" in line and "meters" in line:
+                    output_file.write(line.split(": ")[1].split(" m")[0] + ',\n')
